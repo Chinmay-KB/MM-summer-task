5736f4cfcefd3c0ad94c94dbd8e51ce50cbaf7c5
Chinmay Kabi <chinmay.kabi@live.com>
2018-05-16 22:50:12 +0530

    just a typo

--- MM-app/app/src/main/java/mm/kb/com/mondaymorning/SignIn.java
+++ MM-app/app/src/main/java/mm/kb/com/mondaymorning/SignIn.java
@@ -1,13 +1,13 @@
 package mm.kb.com.mondaymorning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SignIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.signin_page);
     }
+
 }
